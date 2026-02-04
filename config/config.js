ac7a014a1ecc315059999389ce19c990460466c7
likhontheoldstyle <likhon6t9@proton.me>
2026-02-04 20:04:41 +0600

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  BOT_TOKEN: "8447574444:AAHxyNN-92b9N8BZl9JVamX_giIwT9TnFKI",
+
+  BOT_SETTINGS: {
+    NAME: "✨⟦ 𝐋𝐈𝐊𝐇𝐎𝐍 𝐁𝐎𝐓 ⟧✨",
+    VERSION: "2.0.1.9",
+    PREFIX: "/",
+    ADMINS: ["8287206585"],
+    ADMIN_FACEBOOK_URL: "https://www.facebook.com/LIKHONAHMED009",
+    ADMIN_WHATSAPP: "wa.me/+8801761838316",
+    CREDIT: "LIKHON AHMED"
+  },
+
+  OWNER: {
+    ID: 8287206585,
+    USERNAME: "LIKHONAHMED009"
+  },
+
+  REQUIRED_CHATS: [
+    {
+      id: -1003319296127,
+      username: "likhon_premium",
+      name: "✨⟦ 𝐗𝟐𝟎 - 𝐔𝐏𝐃𝐀𝐓𝐄𝐒 ⟧✨"
+    },
+    {
+      id: -1002710357307,
+      username: "likhon_x_booms_apk",
+      name: "𝐋𝐈𝐊𝐇𝐎𝐍 𝐗 𝐁𝐎𝐎𝐌𝐒 𝐀𝐏𝐊 💀"
+    }
+  ]
+};
